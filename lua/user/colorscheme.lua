02593680f3dd49ab6e29965e91bd2e8fe5e62200
JakeSwin <jacob.swindell@btinternet.com>
2022-07-06 15:08:15 +0100

    added dracula colorscheme

---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,9 @@
+local colorscheme = "dracula"
+
+vim.g.dracula_transparent_bg = true
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
