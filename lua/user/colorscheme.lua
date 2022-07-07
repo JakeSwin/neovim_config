0485b044f91a4580c731ed5ae0dd1f9001eba1a0
JakeSwin <jacob.swindell@btinternet.com>
2022-07-07 14:00:21 +0100

    fix colorscheme + add completion

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,7 @@
 local colorscheme = "dracula"
 
-vim.g.dracula_transparent_bg = true
+-- vim.g.dracula_transparent_bg = true
+-- vim.g.dracula_italic_comment = true
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
